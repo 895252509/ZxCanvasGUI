69b9a6b863a84691ce7ef66010aaee9360a40946
Zx <895252509@qq.com>
2017-02-28 22:25:13 +0800

    同步进度
    将目前的进度同步到Git，方便管理

---
+++ script/a.js
@@ -0,0 +1,50 @@
+var DEBUGGER = true;
+
+window.onload = function(){
+    
+    if(DEBUGGER) debugger;
+
+    console.log( Zx.Util.getClientSize() );
+    
+    Zxc.info();
+    
+    var ican = new Zxc.ZxCanvas();
+    ican.Initialization('z-canvas');
+    
+    var a=  new Zxc.Item({
+        style:{
+            style:{
+                x:0,
+                y:0,
+                w:200,
+                h:300
+            }
+        }
+    });
+    ican.add(a);
+    ican.show();
+
+    ican.clear({
+        x:0,
+        y:0,
+        w:20,
+        h:30
+    });
+    
+    var lable = new Zxc.UI.Lable({
+        style:{
+            style:{
+                x:0,
+                y:0
+            }
+        },
+        listeners:{
+            oninit:function (){
+                console.log('lable oninit');
+            }
+        }
+    });
+    ican.add(lable);
+    ican.show();
+    
+}
